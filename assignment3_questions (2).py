b7a885f633743434677f2eb5599427e91500a441
Hassan_Sait <93897019+hassan-y-maersk@users.noreply.github.com>
2025-05-31 13:14:53 +0530

    Add files via upload

---
+++ assignment3_questions (2).py
@@ -0,0 +1,87 @@
+
+
+
+# Olympics Dataset 
+This data is a publicly available. The ***olympix_data.csv*** file is in the Blackboard.
+
+# Dataset Description
+
+This data set consists of the following fields:
+
+***Athlete [0]***: Name of the athlete
+
+***Age [1]***: Age of the athlete
+
+***Country [2]***: The name of the country participating in Olympics
+
+***Year [3]***: The year in which Olympics is conducted
+
+***Closing Date [4]***: Closing date of Olympics
+
+***Sport [5]***: Sports name
+
+***Gold Medals [6]***: No. of gold medals
+
+***Silver Medals [7]***: No. of silver medals
+
+***Bronze Medals [8]***: No. of bronze medals
+
+***Total Medals [9]***: Total no. of medals
+"""
+
+import findspark
+findspark.init()
+
+# create entry points to spark
+
+"""## Q1: Write Pandas  code to read olympix_data.csv file"""
+
+#read the data
+
+"""## Q2: Write Pandas code to print the Olympic Sports/games in the dataset."""
+
+
+
+"""![alt text](https://)## Q3: Write Pandas code to plot the total number of medals in  each Olympic Sport/game. Sort the result based on the the total number of medals."""
+
+
+
+"""### Q4: Find the total number of medals won by each country in swimming.
+
+1.   List item
+2.   List item
+
+
+
+*   List item
+
+*   List item
+
+*   List item
+*   List item
+
+
+*   List item
+
+
+*   List item
+"""
+
+
+
+"""### Q5: Find the total number of medals won by each country in Skeleton."""
+
+
+
+"""### Q6 Find the number of medals that the US won yearly."""
+
+
+
+"""### Q7 Find the total number of medals won by each country."""
+
+
+
+"""### Q8 Who was the oldest athletic in the olympics? whcih country was he/she from?"""
+
+
+
